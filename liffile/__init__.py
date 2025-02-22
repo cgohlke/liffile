2440d87fdbcdcfd7cdf67582b826ad0d5f2aa182
Christoph Gohlke <cgohlke@cgohlke.com>
2025-02-22 10:31:19 -0800

    Update liffile/__init__.py

--- liffile/__init__.py
+++ liffile/__init__.py
@@ -1,5 +1,4 @@
 # liffile/__init__.py
-# pylint: skip-file
 
 from .liffile import *
 from .liffile import __all__, __doc__, __version__
@@ -8,3 +7,16 @@ from .liffile import __all__, __doc__, __version__
 
 __version__ = __version__
 """Liffile version string."""
+
+
+def _set_module() -> None:
+    """Set __module__ attribute for all public objects."""
+    globs = globals()
+    module = globs['__name__']
+    for item in __all__:
+        obj = globs[item]
+        if hasattr(obj, '__module__'):
+            obj.__module__ = module
+
+
+_set_module()
